30c223cd055d90a8a39419d89e733f2b61a8c9c0
Sasha Mishcheriakova <135987917+sashamishcheriakova@users.noreply.github.com>
2024-04-25 11:34:31 +0100

    New translations main.py (French)

--- fr-FR/code/codebreaker_three_language/main.py
+++ fr-FR/code/codebreaker_three_language/main.py
@@ -16,98 +16,98 @@ def creer_code():
 
     # print(code)
 
-# Calculate the frequency of all letters in a piece of text
-def frequency(text):
-    text = list(text.lower())  # Lowercase the message and make it a list
+# Calculer la fréquence de toutes les lettres d'un texte
+def frequence(texte):
+    texte = list(texte.lower())  # Mettre le message en minuscules et en faire une liste
 
-    freq = {}  # Create a dict of every letter, with a count of 0
-    for letter in alphabet:
-        freq[letter] = 0
+    freq = {} # Créer un dictionnaire pour chaque lettre, avec un compte de 0
+    for lettre in alphabet:
+        freq[lettre] = 0
 
-    total_letters = len(text)  # Count the letters in the message
+    total_lettres = len(texte)  # Compter les lettres du message
 
-    for letter in text:
-        if letter in freq:
-            freq[letter] += 1
+    for lettre in texte:
+        if lettre in freq:
+            freq[lettre] += 1
 
-    for letter in freq:  # Convert from counts to percentages
-        freq[letter] = freq[letter] / total_letters * 100
+    for lettre in freq:  # Convertir les comptages en pourcentages
+        freq[lettre] = freq[lettre] / total_lettres * 100
 
     return freq
 
-# Make frequency chart
-def make_chart(text, language):
-    chart = Bar(width=800, title='Frequency analysis',
-                x_labels=list(text.keys()))
-    # First explicit use of values
-    chart.add('Target message', list(text.values()))
-    chart.add('Language', list(language.values()))
+# Faire un diagramme de fréquence
+def faire_graphique(texte, langue):
+    graphique = Bar(width=800, title='Analyse fréquentielle',
+                x_labels=list(texte.keys()))
+    # Première utilisation explicite des valeurs
+    chart.add('Message cible', list(texte.values()))
+    chart.add('Langue', list(langue.values()))
 
-    chart.render()
-
-
-# Encode/decode a piece of text — atbash is symetrical
-def atbash(text):
-    text = text.lower()  #  Converting text to lowercase
-    output = ''
-
-    for letter in text:
-        if letter in code:
-            # Populate output with the encoded/decoded message using the dictionary
-            output += code[letter]
-
-    return output  # Return the encoded/decoded message
+    graphique.render()
 
 
-# Fetch and return text from a file
-def get_text(filename):
+# Coder/décoder un morceau de texte - atbash est symétrique
+def atbash(texte):
+    texte = texte.lower()  #  Convertit le texte en minuscules
+    sortie = ''
+
+    for lettre in texte:
+        if lettre in code:
+            # Remplit la sortie avec le message codé/décodé en utilisant le dictionnaire
+            sortie += code[lettre]
+
+    return sortie  # Retourner le message codé/décodé
+
+
+# Récupérer et renvoyer le texte d'un fichier
+def obtenir_texte(filename):
     with open(filename) as f:
-        text = f.read().replace('\n', '')  # Need to strip the newline characters
+        texte = f.read().replace('\n', '')  # Nécessité de supprimer les caractères de retour à la ligne
 
-    return text
+    return texte
 
-# Create a text-based menu system
+# Créer un système de menu basé sur du texte
 def menu():
-    choice = ''  # Start with a wrong answer for choice.
+    choix = ''  # Commence par une mauvaise réponse pour le choix.
 
-    while choice != 'c' and choice != 'f' and choice != 'm':  # Keep asking the user for the right answer
-        choice = input(
-            'Please enter c to encode/decode a text file, f to perform frequency analysis in three languages, or m to enter your own message to encode:')
+    while choix != 'c' and choix != 'f' and choix != 'm':  # Continuer à demander à l'utilisateur la bonne réponse
+        choix = input(
+            'Saisis c pour coder/décoder un fichier texte, f pour effectuer une analyse fréquentielle en trois langues, ou m pour saisir ton propre message à coder :')
 
-    if choice == 'c':
-        print('Running your message through the cypher…')
-        message = get_text('longer.txt')  # Take input from a file
+    if choix == 'c':
+        print('Nous passons ton message dans le cryptogramme…')
+        message = obtenir_texte('longer.txt')  # Prendre l'entrée d'un fichier
         code = atbash(message)
         print(code)
 
-    elif choice == 'f':
-        print('Analysing message…')
-        # Take input from the same file. We have a 'longer.txt' or similar containing cyphertext we know to perform reasonably well for frequency analysis
-        message = get_text('longer.txt')
-        # Get the frequency of the letters in the message, as %
+    elif choix == 'f':
+        print('Analyse du message…')
+        # Prend l'entrée du même fichier. Nous disposons d'un fichier 'longer.txt' ou d'un fichier similaire contenant un texte crypté dont nous savons qu'il se prête assez bien à l'analyse fréquentielle
+        message = obtenir_texte('longer.txt')
+        # Obtenir la fréquence des lettres dans le message, en %.
         message_freq = frequency(message)
         # print(message_freq)
-        language = input(
-            'Which language is your message in? \n1. English \n2. French \n3. Spanish')
+        langue = input(
+            'Dans quelle langue ton message est-il rédigé ? \n1. Anglais \n2. Français \n3. Espagnol')
 
-        if language == '1':
-            lang_freq = english  # Import the English frequency dictionary
-        elif language == '2':
-            lang_freq = french
-        elif language == '3':
-            lang_freq = spanish
+        if langue == '1':
+            lang_freq = anglais  # Importation du dictionnaire de fréquence anglais
+        elif langue == '2':
+            lang_freq = francais
+        elif langue == '3':
+            lang_freq = espagnol
 
-        # Call the function to make a chart
-        make_chart(message_freq, lang_freq)
+        # Appeler la fonction pour faire un graphique
+        faire_graphique(message_freq, lang_freq)
 
-    elif choice == 'm':
-        message = input('What text would you like to encode?')
+    elif choix == 'm':
+        message = input('Quel texte veux-tu coder ?')
         code = atbash(message)
         print(code)
 
-# Start up
+# Démarrage
 def main():
-    create_code()
+    creer_code()
     menu()
 
 
